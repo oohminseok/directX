1fb03b39ed06cda650bb67ca4d32fb3625b2471d
OHMINSEOK <116791454+oohminseok@users.noreply.github.com>
2023-10-16 17:26:50 +0900

    Update GameLogic.h

--- Card/GameLogic.h
+++ Card/GameLogic.h
@@ -2,30 +2,34 @@
 #include <list>
 #include "Card.h"
 
-namespace solitaire
+class GameLogic
 {
-	class GameLogic
-	{
-		const int BOARD_ROW = 8;
-		const int BOARD_COLUMN = 5;
-		const Gdiplus::RectF mCountRect{ 885.0f,60.0f,120.0f,30.0f };
+	const int BOARD_ROW = 8;
+	const int BOARD_COLUMN = 2;
+	const Gdiplus::RectF mCountRect1{ 885.0f,60.0f,120.0f,30.0f };
+	const Gdiplus::RectF mCountRect2{ 885.0f,440.0f,120.0f,30.0f };
 
-	private:
-		std::unique_ptr<Gdiplus::Image> mBackground;
-		std::list<Card> mDeck;
+private:
+	std::unique_ptr<Gdiplus::Image> mBackground;
+	std::list<Card> mDeck1;
+	std::list<Card> mDeck2;
 
-		HWND mHwnd;
-		int mFlipCount{};
-		Card* mpSelcetedCard{};
+	HWND mHwnd{};
+	Gdiplus::Graphics* mGraphics{};
+	int mFlipCount1{};
+	int mFlipCount2{};
+	bool mPlayer1{ false };
+	bool mPlayer2{ true };
+	Card* mpSelectedCard1{};
+	Card* mpSelectedCard2{};
 
-	public:
-		void Init(HWND hwnd);
-		void Release();
-		void Draw(Gdiplus::Graphics& graphics);
-		void OnClick(int x, int y);
+public:
+	void Init(HWND hwnd);
+	void Release();
+	void Draw(Gdiplus::Graphics& graphics);
+	void OnClick(int x, int y);
+	Gdiplus::Graphics* SetGraphics(Gdiplus::Graphics& graphics);
 
-	private:
-		void CreateCards();
-	};
-}
-
+private:
+	void CreateCards();
+};
